2b455dd6f7254d50fc8fc57f3cf126411b6716b4
kamilmyskow <73378256+kamilmyskow@users.noreply.github.com>
2021-05-24 18:32:40 +0200

    Add files via upload
    Etap 1 - optymalizacja kodu

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -6,13 +6,17 @@ using namespace std;
 int spr_max (int a,int b,int c)
 {
 	int maximum;
-	if (a > b) maximum = a;
+	
+	if ((a > b) && (a > c)) maximum = a; //optymalizacja kodu 
+	if ((b > a) && (b > c)) maximum = b; //mniej linii daje większą czytelność, mniej warunków lepszą skuteczność
+	if ((c > a) && (c > b)) maximum = c; 
+/*	if (a > b) maximum = a;
 	if (a > c) maximum = a;
 	if (a < b) maximum = b;
 	if (a < c) maximum = c;
 	if (b > c) maximum = b;
 	if (c > b) maximum = c;
-
+*/
 	return maximum;
 
 }
