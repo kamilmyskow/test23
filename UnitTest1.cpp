492f6fcb80b69ef38f1cf13652957bc8e8daad9c
Sebastian <73378416+sanczo37@users.noreply.github.com>
2021-05-24 18:33:56 +0200

    Add files via upload
    Etap 2 - dopisanie testu do drugiej metody

--- UnitTest1.cpp
+++ UnitTest1.cpp
@@ -1,4 +1,4 @@
-#include "pch.h"
+#inc#include "pch.h"
 #include "CppUnitTest.h"
 #include "../ConsoleApplication1/ConsoleApplication1.cpp"
 #include <iostream>
@@ -16,6 +16,10 @@ namespace UnitTest1
 		TEST_METHOD(is_max) //sprawdzenie czy liczba z przedziału -1=< x =<1 jest max
 		{
 			Assert::AreEqual(spr_max(-1,1,0), 1);
+		}
+		TEST_METHOD(is_min)//sprawdzenie czy liczba z przedziału -5=< x =<5 jest min
+		{
+			Assert::AreEqual(spr_min(-1,2,5), -1);
 		}
 		
 		
